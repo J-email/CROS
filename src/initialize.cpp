f6df366749b758975e3de054ae30744233c92971
jerry <email__address@163.com>
2021-11-25 13:01:57 +0800

    initial commit

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -8,11 +8,6 @@
 void initialize() {
 	controller.rumble("- - -");//rumble 3 times to indicate start of initialization
 	drive->getModel()->setBrakeMode(AbstractMotor::brakeMode::coast);//set the brake mode to coast
-	path_gen->generatePath({ //calculate path
-  {0_m, 0_m, 0_deg},  // Profile starting position, this will normally be (0, 0, 0)
-  {1_m, -1_m, -90_deg},}, // The next point in the profile, 3 feet forward
-  "A" // Profile name
-);
 	pros::lcd::initialize();
 	pros::lcd::set_text(3, "80 International"); //print "80 INT" in the 3rd line
 	controller.rumble(". . .");//rumble 3 times to indicate end of initialization
