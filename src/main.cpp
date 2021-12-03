c547ca8203aa102bd979f2d4035a480b3b01dfdb
J-email <email__address@163.com>
2021-12-03 14:49:31 +0800

    PID

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,13 @@ std::shared_ptr<ChassisController> drive =
 				// Green gear-set, 4 in wheel diam, 11.5 in wheel track
 				.withDimensions(AbstractMotor::gearset::green, {{WHEEL_DIAMETER, WHEEL_TRACK}, imev5GreenTPR})
 				.withMaxVoltage(VOLTAGE_LIMIT)
+                .withGains(
+                        {0, 0, 0}, // Distance controller gains
+                        {0, 0, 0}, // Turn controller gains
+                        {0, 0, 0}  // Angle controller gains (helps drive straight)
+                        )
                 .withOdometry()
 				.buildOdometry();
-
-std::shared_ptr<AsyncMotionProfileController> path_gen =
-		AsyncMotionProfileControllerBuilder()
-				.withLimits({
-				      1.0, // Maximum linear velocity of the Chassis in m/s
-				      2.0, // Maximum linear acceleration of the Chassis in m/s/s
-				      10.0 // Maximum linear jerk of the Chassis in m/s/s/s
-				    })
-				.withOutput(drive)
-				.buildMotionProfileController();
 
 //controller
 Controller controller;
